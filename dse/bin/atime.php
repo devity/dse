7b34f67a373164a64861e8d11dbe76114e93d032
Louis Paul Marquette <marqul@gmail.com>
2012-04-25 11:06:15 -0400

    symlinks test

--- dse/bin/atime.php
+++ dse/bin/atime.php
@@ -8,15 +8,17 @@ include_once ("dse_config.php");
 
 
 
-if($argv[1]=="-r"){
-	$file=$argv[2];
+if($argv[1]=="-?"){
+/*	$file=$argv[2];
 	$sa=stat($file);
+	print "file: $file\n";
+	print_r($sa);
+	print "\n";
 	$Size_str=dse_file_size_to_readable($sa[9]);
-	print $Size_str."\n";;
+	print $Size_str."\n";;*/
 }else{
 	$file=$argv[1];
 	$sa=stat($file);
 	print $sa[9];
 }
-	
 ?>
