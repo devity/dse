8f7f0dbf3bdefed299e949cb50b310e0e1917d69
Louis Paul Marquette <marqul@gmail.com>
2012-06-18 11:51:56 -0400

    dev

--- dse/bin/dse_config_functions.php
+++ dse/bin/dse_config_functions.php
@@ -578,20 +578,21 @@ function dse_configure_create_httpd_conf(){
 	
 	$i=1;
 	foreach($vars['DSE']['SERVER_CONF']['Domains'] as $Domain){
-		//if($i>2) break;
+		if($i>4) break;
 		$domain=strtolower($Domain);
 		$DocRoot=$vars['DSE']['HTTP_ROOT_DIR'];
 		print "$domain *****\n";
 		
 		foreach ($vars['DSE']['SERVER_CONF']['Webroots'][$Domain] as $Hosts=>$Webroot){
-			//if($i>2) break;
+			if($i>4) break;
 			foreach(split(",",$Hosts) as $Host){
-				//if($i>2) break;
+				if($i>4) break;
 				$ServerAlias="$Host.$Domain";
 				if($Host=="_blank") $ServerAlias="$Domain";
 				$IP=$vars['DSE']['SERVER_CONF']['Hosts'][$Domain][$Host];
 				$site="
 <VirtualHost $IP:80>
+ ServerName $Host.$domain
  ServerAlias $ServerAlias
  DocumentRoot $DocRoot/$Webroot
  ErrorLog /var/log/apache2/error.log
@@ -611,13 +612,13 @@ function dse_configure_create_httpd_conf(){
 			
 			$r=`a2ensite $Host.$domain`;
 			print $r;
-			
-			//if($i>2) break;
+		
+			if($i>4) break;
 			$i++;
 			}
-			//if($i>2) break;
+			if($i>4) break;
 		}
-	//if($i>2) break;
+	if($i>4) break;
 	}
 	dse_service_start("httpd");
 }
