c3e436e957ef62a8ff89f95b4d78227fa9adac3a
Louis Paul Marquette <marqul@gmail.com>
2012-06-22 09:30:49 -0400

    code_explorer fix test

--- dse/bin/code_explorer.php
+++ dse/bin/code_explorer.php
@@ -4,6 +4,7 @@ error_reporting(E_ALL && ~E_NOTICE);
 ini_set('display_errors','On');	
 include_once ("/dse/bin/dse_cli_functions.php");
 include_once ("/dse/bin/dse_config.php");
+include_once ("/dse/include/web_functions.php");
 include_once ("/dse/include/code_functions.php");
 $vars['Verbosity']=1;
 
