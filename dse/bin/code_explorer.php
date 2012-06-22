49c95ef8342873abb7d5cfc12c782381d4b72e33
Louis Paul Marquette <marqul@gmail.com>
2012-06-22 10:03:18 -0400

    code_explorer fix test

--- dse/bin/code_explorer.php
+++ dse/bin/code_explorer.php
@@ -22,6 +22,8 @@ $parameters_details = array(
   array('v','verbosity:',"0=none 1=some 2=more 3=debug"),
  // array('p:','parse:',"parses code-base at argv[1]"),
   array('f:','function-declarations:',"shows functions declared in code-base at argv[1]"),
+  array('u:','file-info:',"code-base at argv[1] file-info on file = argv[2]"),
+  
 );
 $vars['parameters']=dse_cli_get_paramaters_array($parameters_details);
 $vars['Usage']=dse_cli_get_usage($parameters_details);
@@ -54,6 +56,89 @@ foreach (array_keys($vars['options']) as $opt) switch ($opt) {
   	case 'function-declarations':
 		print dse_code_return_function_declarations($vars['options'][$opt]);
 		$DidSomething=TRUE;
+		break;
+		
+	case 'file-info':
+		$DidSomething=TRUE;
+		{
+			$CodeBaseDir=$vars['options'][$opt];
+			$CodeInfoArray=dse_code_parse($CodeBaseDir);
+
+		
+			$FileFullName=$argv[1];
+			print "<center><b class='f10pt'>Info on File: $FileFullName</b></center></br><br>";
+			//$FileArray=$CodeInfoArray['Files'][$File]['FileCodeInfoArray']['Functions']['Def'];//
+			//print debug_tostring($FileArray);
+			
+			print "<table width=100%><tr class='f8pt'>";
+			
+			
+			print "<td valign=top><b class='f10pt'>Functions Declared:</b><br>";
+				ksort($CodeInfoArray['Files'][$FileFullName]['FileCodeInfoArray']['Functions']['Def']);
+				foreach($CodeInfoArray['Files'][$FileFullName]['FileCodeInfoArray']['Functions']['Def'] as $n=>$e){
+					$p=$e[3];
+					print "<b class='f9pt'>$n</b> ($p)<br>";
+					// $f:$l
+				}
+			print "</td>";
+			
+			print "<td valign=top><b class='f10pt'>Functions Referenced:</b><br>";
+				ksort($CodeInfoArray['Files'][$FileFullName]['FileCodeInfoArray']['Functions']['Used']);
+				foreach($CodeInfoArray['Files'][$FileFullName]['FileCodeInfoArray']['Functions']['Used'] as $n=>$e){
+					$l=$e[1]; $p=$e[3]; 
+					$Def=$CodeInfoArray['Functions']['Def'][$n];
+					//print "<b class='f9pt'>$n</b>($p) Line:$l<br>";
+					//print "<a><b class='f9pt'>$n</b>($p) Line:$l</a><div>Stuff shown on hover</div><br>";
+					print "<div href=\"#\" class=\"showhim\"><b class='f9pt'>$n</b>($p) Line:$l</a>";
+					print "<div class=\"showme\">";
+					print debug_tostring($Def);
+					print "</div></div>";
+		
+					// $f:$l
+				}
+			print "</td>";
+			
+			print "</tr></table>";
+			
+			
+			print "<br><b class='f10pt'>File Contents:</b><br>";
+			foreach($CodeInfoArray['Files'][$FileFullName]['FileCodeInfoArray']['LinesParsed'] as $LineNumber=>$Line){
+				print "<font color=grey>$LineNumber </font> $Line<br>";
+			}
+	/*
+
+	print "<table width=100%><tr class='f8pt'>";
+	
+	print "<td valign=top><b>Files:</b><br>";
+		foreach($CodeInfoArray['Files'] as $FileFullName=>$FileEntry){
+			$LineCount=$FileEntry['FileCodeInfoArray']['LineCount'];
+			print "<a href=/code_explorer/?FileInfo&File=$FileFullName><b class='f9pt'>$FileFullName</b></a> ($LineCount)<br>";
+			// $f:$l
+		}
+		print "</td>";
+	print "</td>";
+	
+	
+	print "<td valign=top><b>Functions:</b><br>";
+	//print text2html(dse_code_return_function_declarations("/dse"))."<br>";
+	
+		ksort($CodeInfoArray['Functions']['Def']);
+		foreach($CodeInfoArray['Functions']['Def'] as $k=>$fde){
+			$f=$fde[0];
+			$l=$fde[1];
+			$n=$fde[2];
+			$p=$fde[3];
+			$d=$fde[4];
+			print "<b class='f9pt'>$n</b> ($p)<br>";
+			// $f:$l
+		}
+		print "</td>";
+	
+	print "</tr></table>";*/
+
+
+			
+		}
 		break;
 }
 
