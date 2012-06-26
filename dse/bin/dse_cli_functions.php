a1769ac9572cf6552091d7fe6385869b6223069d
Louis Paul Marquette <marqul@gmail.com>
2012-06-26 02:19:48 -0400

    lgt

--- dse/bin/dse_cli_functions.php
+++ dse/bin/dse_cli_functions.php
@@ -993,7 +993,7 @@ function unk_time($TimeAndDateString){
 		$TimeAndDateString=str_replace("st, ",", ",$TimeAndDateString);
 		$TimeAndDateString=str_replace("rd, ",", ",$TimeAndDateString);
 		$TimeAndDateString=strcut($TimeAndDateString,""," EDT");
-		 $len=52; $format = '%a %B %d, %Y, %H:%M.%S %P';
+		 $len=52; $format = '%a %B %d, %Y, %h:%M.%S %P';
 	}//	Sun June 24th, 2012, 5:46.48 am EDT
 	elseif( preg_match ("/^[a-zA-Z]{3} [a-zA-Z]{0,9} [0-9]{1,2}, [0-9]{4}, [0-9]{1,2}:[0-9]{2}.[0-9]{2} [a-zA-Z]{2}/" , $TimeAndDateString, $matches) >0 ){
 		dpv(5,"preg_match 8b \n");
