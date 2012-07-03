45a0f029cd626c52adfffb07847716206c92cdd1
Louis Paul Marquette <marqul@gmail.com>
2012-07-03 08:51:59 -0400

    no_message

--- dse/bin/dse_cli_functions.php
+++ dse/bin/dse_cli_functions.php
@@ -1858,6 +1858,10 @@ function unk_time($TimeAndDateString){
 			$TimeAndDateString=strcut($TimeAndDateString,"","DT");
 		}
 		 $len=52; $format = '%a %B %d, %Y, %l:%M.%S %P';
+		 
+list($DayName,$MonthName,$Date,$Year,$Hour,$Minute,$Second,$AMPM) = sscanf($TimeAndDateString, "%s %s %d, %d, %d:%d.%d %s");
+print "d=$DayName,m=$MonthName,dt=$Date,y=$Year,h=$Hour,m=$Minute,s=$Second,ampm=$AMPM\n";
+
 	}//	Sun June 24th, 2012, 5:46.48 am EDT
 	elseif( preg_match ("/^[a-zA-Z]{3} [a-zA-Z]{0,9} [0-9]{1,2}, [0-9]{4}, [0-9]{1,2}:[0-9]{2}.[0-9]{2} [a-zA-Z]{2}/" , $TimeAndDateString, $matches) >0 ){
 		dpv(5,"preg_match 8b \n");
