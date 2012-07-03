8066e152c1b9a8513da4868c4e0618e47e7da9e7
Louis Paul Marquette <marqul@gmail.com>
2012-07-03 00:17:12 -0400

    no_message

--- dse/bin/dfm.php
+++ dse/bin/dfm.php
@@ -310,15 +310,17 @@ if($DoLineWithString){
 	//$Message=" numbering lines in $File :\n";
 	//dpv(1,$Message);
 	//dse_log($Message);
-	$FileContents=dse_file_get_contents($File);
+//	$FileContents=dse_file_get_contents($File);
+	$Command="grep -n \"$String\" $File";
+	$r=dse_exec($Command);
 	$Line=0;
 	$tbr="";
-	foreach(split("\n",$FileContents) as $Line){
+	foreach(split("\n",$r) as $Line){
 		$i++;
-		if(str_contains($Line,$String)){
+		//if(str_contains($Line,$String)){
 			if($tbr)$tbr.=",";
-			$tbr.= "$i";
-		}
+			$tbr.= "$i $Line";
+		//}
 	}
 	print $tbr;
 	exit(0);
