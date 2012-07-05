a49b5f7e206135f909d952855c6206fc18ad72b0
Louis Paul Marquette <marqul@gmail.com>
2012-07-04 22:13:48 -0400

    no_message

--- dse/include/system_stat_functions.php
+++ dse/include/system_stat_functions.php
@@ -881,11 +881,13 @@ function dse_sysstats_httpd_fullstatus(){
 			$Lpa=split(" ",$LastLine);
 			if($Lpa[14]!="NULL"){
 				$URL=$Lpa[13]."/".$Lpa[15];
-				$Mode=$Lpa[3];
-				$CPU=$Lpa[4];
-				$SS=$Lpa[5];
-				print_r($Lpa);
-				print "\n cpu:$CPU cc:$SS m:$Mode ".$LastLine."\n";
+				$Mode=$Lpa[2];
+				$CPU=$Lpa[3];
+				$SS=$Lpa[4];
+				$Req=$Lpa[5];
+				$Con=$Lpa[6];
+				//print_r($Lpa);
+				print "\n cpu:$CPU cc:$SS m:$Mode req:$Req con:$Con ".$LastLine."\n";
 			}
 			$LastLine="";
 			$LastLine="PID: $PID ";
