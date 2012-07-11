c84aa99eda1d945224f041d398618e2ec71664dd
Louis Paul Marquette <marqul@gmail.com>
2012-07-10 20:31:43 -0400

    no_message

--- dse/bin/dse-install.php
+++ dse/bin/dse-install.php
@@ -423,7 +423,7 @@ if(!in_array($ComponentName, $vars['DSE']['DisabledComponents'])){
 			$NotOSXPackageNamesArray[]="apache2-utils";
 		}
 		
-		$PackageNamesArray[]="munin";
+		//$PackageNamesArray[]="munin";
 		$NotOSXPackageNamesArray[]="mpstat";
 		$PackageNamesArray[]="phoronix";
 		$PackageNamesArray[]="cpuburn";
